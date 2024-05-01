5eb13cf359c77fecddc1e6165f026b8ad78fd8fb
Micha de Groot <micha.degroot@braincreators.com>
2024-05-01 15:34:13 +0200

    doc: notes for more plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,6 +41,18 @@ require('lazy').setup({
 
   require 'kickstart.plugins.neo-tree',
 
+  -- harpoon
+
+  -- undotree
+
+  -- 'linux-cultist/venv-selector.nvim'
+
+  -- lazygit
+
+  -- copilot
+
+  -- nerdtree instead of neo-tree ?
+
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
