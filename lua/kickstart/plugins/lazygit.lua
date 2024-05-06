e154e7b093383d4e05d9cdbafbde21215f4af2d1
Micha de Groot <micha.degroot@braincreators.com>
2024-05-06 13:54:12 +0200

    fix: plugins

--- lua/kickstart/plugins/lazygit.lua
+++ lua/kickstart/plugins/lazygit.lua
@@ -6,7 +6,7 @@ return {
     },
     config = function()
         require('telescope').load_extension 'lazygit'
-        vim.g.lazygit_floating_window_winblend.set(1.0),                                                               -- Transparency of floating window
+        vim.g.lazygit_floating_window_winblend.set(1.0)                                                               -- Transparency of floating window
         vim.g.lazygit_floating_window_use_plenary.set(1)                                                               -- Use plenary.vim to manage floating window
         vim.keymap.set('n', '<leader>gg', ':LazyGit<cr>', { silent = true })
         vim.keymap.set('n', '<leader>flg', 'require("telescope").extensions.lazygit.lazygit()<cr>', { silent = true }) -- don't quite remember what this does
