3b4584524cffc2a0aa4183c717804b62ae8f4a63
Micha de Groot <micha.degroot@braincreators.com>
2024-05-10 14:24:02 +0200

    fix(plugins): remove unneeded setting

--- lua/kickstart/plugins/venv-selector.lua
+++ lua/kickstart/plugins/venv-selector.lua
@@ -6,7 +6,6 @@ return { -- Select a virtual env automatically
     -- name = "venv",
     -- auto_refresh = false
   },
-  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
     { '<leader>vs', '<cmd>VenvSelect<cr>' },
