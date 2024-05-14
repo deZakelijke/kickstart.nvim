4a56854646b0d13afdc528d712f398698e6c4073
Micha de Groot <micha.degroot@braincreators.com>
2024-05-14 11:26:18 +0200

    feat(plugins): enable mini

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -33,7 +33,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.todo-comments',
 
-  -- require 'kickstart.plugins.mini',
+  require 'kickstart.plugins.mini',
 
   require 'kickstart.plugins.treesitter',
 
