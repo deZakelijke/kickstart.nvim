db2ab162f1db6eb44b97cb6f10ee50a44cdc12c6
Micha de Groot <micha.degroot@braincreators.com>
2024-05-21 15:43:00 +0200

    fix(plugins): set keybindings for copilot chat

--- lua/kickstart/plugins/copilot_chat.lua
+++ lua/kickstart/plugins/copilot_chat.lua
@@ -1,21 +1,15 @@
 return { -- Chat with Copilot
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
-    dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = false, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    config = function()
-      require('copilot-chat').setup()
-      vim.keymap.set('n', '<leader>cct', vim.cmd.CopilotChatToggle, { desc = '[C]opilot [C]hat [T]oggle' })
-      vim.keymap.set('n', '<leader>cce', vim.cmd.CopilotChatExplain, { desc = '[C]opilot [C]hat [E]xplain' })
-      vim.keymap.set('n', '<leader>ccr', vim.csm.CopilotChatReview, { desc = '[C]opilot [C]hat [R]eview' })
-    end,
-    -- See Commands section for default commands if you want to lazy load on them
+  'CopilotC-Nvim/CopilotChat.nvim',
+  branch = 'canary',
+  dependencies = {
+    { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+    { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
   },
+  config = function()
+    require('CopilotChat').setup()
+    vim.keymap.set('n', '<leader>cc', vim.cmd.CopilotChatToggle, { desc = '[C]opilot [C]hat toggle' })
+    vim.keymap.set('n', '<leader>ce', vim.cmd.CopilotChatExplain, { desc = '[C]opilot [E]xplain code selection' })
+    vim.keymap.set('n', '<leader>cd', vim.cmd.CopilotChatDocs, { desc = '[C]opilot write [D]ocs for selection' })
+    vim.keymap.set('n', '<leader>ct', vim.cmd.CopilotChatTests, { desc = '[C]opilot write [T]ests for code' })
+  end,
 }
