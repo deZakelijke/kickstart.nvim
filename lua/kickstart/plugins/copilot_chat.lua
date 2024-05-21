7a1cf7e42ce3808d029c01f4f281d706c90cf512
Micha de Groot <micha.degroot@braincreators.com>
2024-05-21 15:47:12 +0200

    chore(plugins): add copilot keybindings for visual mode

--- lua/kickstart/plugins/copilot_chat.lua
+++ lua/kickstart/plugins/copilot_chat.lua
@@ -9,7 +9,10 @@ return { -- Chat with Copilot
     require('CopilotChat').setup()
     vim.keymap.set('n', '<leader>cc', vim.cmd.CopilotChatToggle, { desc = '[C]opilot [C]hat toggle' })
     vim.keymap.set('n', '<leader>ce', vim.cmd.CopilotChatExplain, { desc = '[C]opilot [E]xplain code selection' })
+    vim.keymap.set('v', '<leader>ce', vim.cmd.CopilotChatExplain, { desc = '[C]opilot [E]xplain code selection' })
     vim.keymap.set('n', '<leader>cd', vim.cmd.CopilotChatDocs, { desc = '[C]opilot write [D]ocs for selection' })
+    vim.keymap.set('v', '<leader>cd', vim.cmd.CopilotChatDocs, { desc = '[C]opilot write [D]ocs for selection' })
     vim.keymap.set('n', '<leader>ct', vim.cmd.CopilotChatTests, { desc = '[C]opilot write [T]ests for code' })
+    vim.keymap.set('v', '<leader>ct', vim.cmd.CopilotChatTests, { desc = '[C]opilot write [T]ests for code' })
   end,
 }
