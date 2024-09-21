5a3d5be6a8ba0b5f1dd7ff9073cc1beed47bb2d7
deZakelijke <git@michadegroot.nl>
2024-09-21 17:43:17 +0200

    chore: disabled some unused keymaps

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -9,19 +9,25 @@ return { -- Useful plugin to show you pending keybinds.
     require('which-key').setup()
 
     -- Document existing key chains
-    require('which-key').register {
-      -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-    }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
+    -- NOTE: these are probably wrong
+    -- require('which-key').register {
+    --   { '', group = '[S]earch' },
+    --   { '', desc = '<leader>w_', hidden = true },
+    --   { '', group = '[T]oggle' },
+    --   { '', desc = '<leader>s_', hidden = true },
+    --   { '', group = '[D]ocument' },
+    --   { '', group = 'Git [H]unk' },
+    --   { '', desc = '<leader>t_', hidden = true },
+    --   { '', desc = '<leader>r_', hidden = true },
+    --   { '', group = '[W]orkspace' },
+    --   { '', desc = '<leader>d_', hidden = true },
+    --   { '', group = '[R]ename' },
+    --   { '', desc = '<leader>h_', hidden = true },
+    -- }
+    -- -- visual mode
+    -- require('which-key').register {
+    --   { '', desc = '<leader>h', mode = 'v' },
+    -- }
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
