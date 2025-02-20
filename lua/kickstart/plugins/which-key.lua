145debf5aba07349344e1cb095955d471f6460a6
Micha de Groot <micha.degroot@braincreators.com>
2025-02-20 09:50:52 +0100

    chore: remove keys from which-key

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -4,31 +4,9 @@
 
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  tag = '2.1.0',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      { '', group = '[S]earch' },
-      { '', desc = '<leader>w_', hidden = true },
-      { '', group = '[T]oggle' },
-      { '', desc = '<leader>s_', hidden = true },
-      { '', group = '[D]ocument' },
-      { '', group = 'Git [H]unk' },
-      { '', desc = '<leader>t_', hidden = true },
-      { '', desc = '<leader>r_', hidden = true },
-      { '', group = '[W]orkspace' },
-      { '', desc = '<leader>d_', hidden = true },
-      { '', group = '[R]ename' },
-      { '', desc = '<leader>h_', hidden = true },
-    }
-    -- visual mode
-    require('which-key').register({
-      { '', desc = '<leader>h', mode = 'v' },
-      { '', desc = '<leader>h', mode = 'v' },
-    }, { mode = 'v' })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
