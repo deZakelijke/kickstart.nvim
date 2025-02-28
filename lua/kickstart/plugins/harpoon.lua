732d86c20809c46b5cd587619ae710c2fa7cd432
Micha de Groot <micha.degroot@braincreators.com>
2025-02-28 12:50:32 +0100

    fix: use correct object property token

--- lua/kickstart/plugins/harpoon.lua
+++ lua/kickstart/plugins/harpoon.lua
@@ -15,35 +15,35 @@ return { -- Move between up to four files with F1-F4
     {
       '<C-e>',
       function()
-        require('harpoon').ui:toggle_quick_menu(require('harpoon').list())
+        require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
       end,
       desc = 'Harpoon [E]xplorer',
     },
     {
       '<leader>hq',
       function()
-        require('harpoon').list():select(1)
+        require('harpoon'):list():select(1)
       end,
       desc = '[H]arpoon Select [Q] first',
     },
     {
       '<leader>hw',
       function()
-        require('harpoon').list():select(2)
+        require('harpoon'):list():select(2)
       end,
       desc = '[H]arpoon Select [W] second',
     },
     {
       '<leader>he',
       function()
-        require('harpoon').list():select(3)
+        require('harpoon'):list():select(3)
       end,
       desc = '[H]arpoon Select [E] third',
     },
     {
       '<leader>hr',
       function()
-        require('harpoon').list():select(4)
+        require('harpoon'):list():select(4)
       end,
       desc = '[H]arpoon Select [R] fourth',
     },
