ac03f9e6f149b231877eefb5e314cd70ba1bfe1d
Micha de Groot <micha@smartworkz.nl>
2026-02-20 11:22:36 +0100

    feat: update Oil settings

--- lua/kickstart/plugins/oil.lua
+++ lua/kickstart/plugins/oil.lua
@@ -8,7 +8,17 @@ return { -- file tree
     },
   },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      default_file_explorer = true,
+      columns = {
+        'icon',
+        'permissions',
+      },
+      watch_for_changes = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
