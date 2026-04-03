6ed8db011c91187c6bccf485d76245d4d5a11221
Micha de Groot <micha@smartworkz.nl>
2026-04-03 14:24:48 +0200

    chore: enable snacks - image

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -7,7 +7,7 @@ return { -- Editor utilities
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    image = { enabled = false },
+    image = { enabled = true },
     lazygit = { enabled = true },
     picker = { enabled = true },
   },
